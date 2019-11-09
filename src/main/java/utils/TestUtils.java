6d966a3f5d4cc0c3be7ec0c9588a598527762ba0
Usha Yadu <ushayadu04@gmail.com>
2019-11-09 16:59:38 +0530

    added expected wait condition

--- src/main/java/utils/TestUtils.java
+++ src/main/java/utils/TestUtils.java
@@ -1,13 +1,27 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class TestUtils {
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 
-	private void waitFor(int durationInMilliSeconds) {
+	public void waitFor(int durationInMilliSeconds) {
 		try {
 			Thread.sleep(durationInMilliSeconds);
 		} catch (InterruptedException e) {
+			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+		}
+	}
+	public void waitUntil(WebElement element, WebDriver driver) {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 60);
+			wait.until(ExpectedConditions.elementToBeClickable(element));
+
+		} catch (Exception e) {
 			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
 		}
 	}
